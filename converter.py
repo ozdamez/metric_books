cce86b7245fe6a7fcfb69c77c12e567e0a523d11
ozdamez <ozdamez@gmail.com>
2023-09-19 22:33:19 +0200

    last commit for 19.09.2023

--- converter.py
+++ converter.py
@@ -1,52 +1,69 @@
-import csv, re
+import csv
 
 # Открываем файл с текстом для чтения
-#input_file_name = '1943.txt'
-#with open(input_file_name, 'r', encoding='utf-8') as input_file:
-#    text = input_file.read()
-text = """Р01.01.1943 Мина Мальцевич. Евдоким Евфимьев и Ирина Федоровна ур. Матарас
-Р01.01.1943 Надежда Вергун. Алексей Никифоров и Екатерина Лаврентьевна ур. Михлюк
-Р03.01.1943 Рубрин Владимир Масло. Филипп Адольфов и София Федоровна ур. Давидовская
-Р07.01.1943 Иван и Петр Михлюк. Александр Иванович и Александра Петровна ур. Дубравская
-Р28.01.1943 толм. Надежда Гананайко. Иван Ильин и Елена Евфимьева ур. Крук
-Р01.02.1943 кор. Мария Савонова. Петр Максимович и Екатерина Косьмина ур. Коваль"""
+input_file_name = '1943.txt'
+with open(input_file_name, 'r', encoding='utf-8') as input_file:
+    text = input_file.read()
+#text = """Р01.01.1943 Мина Мальцевич. Евдоким Евфимьев и Ирина Федоровна ур. Матарас
+#Р01.01.1943 Надежда Вергун. Алексей Никифоров и Екатерина Лаврентьевна ур. Михлюк
+#Р03.01.1943 рубрин Владимир Масло. Филипп Адольфов и София Федоровна ур. Давидовская
+#Р07.01.1943 Иван и Петр Михлюк. Александр Иванович и Александра Петровна ур. Дубравская
+#Р28.01.1943 толм. Надежда Гананайко. Иван Ильин и Елена Евфимьева ур. Крук
+#Р01.02.1943 кор. Мария Савонова. Петр Максимович и Екатерина Косьмина ур. Коваль"""
 
 # Разбиваем текст на строки
 lines = text.split('\n')
-print(lines)
+#print(lines)
 # Создаем список для хранения разделенных данных
 data = []
 
 # Обходим каждую строку и извлекаем нужные данные
 for line in lines:
     if line.strip():  # Пропускаем пустые строки
-        #print(line, "\n")
-        metric_type = line[0]
-        date = line[1:11]
-        names = line[12:].split()
+        try:
+            metric_type = line[0]
+            date = line[1:11]
+            names = line[12:].split()
 
-        if names[0].isupper():
-            village = "Озд."
-            name = names[0]
-            surname = names[1]
-        else:
-            village = names[0]
-            name = names[1]
-            surname = names[2]
+            if names[0][0].isupper():
+                village = "озд."
+                name = names[0]
+                surname = names[1]
+                father = names[2] + ' ' + names[3]
+                mother = names[5] + ' ' + names[6]
+                if names[7]:
+                    if names[7] == "ур.":
+                        maiden = names[8]
+                else:
+                    maiden = ''
 
-        data.append([metric_type, village, date, name, surname])
+            else:
+                village = names[0]
+                name = names[1]
+                surname = names[2]
+                father = names[3] + ' ' + names[4]
+                mother = names[6] + ' ' + names[7]
+                if names[8]:
+                    if names[8] == "ур.":
+                        maiden = names[9]
+                else:
+                    maiden = ''
+
+            data.append([metric_type, village, date, name, surname, father, mother, maiden])
+        except:
+            print(line)
 
 
-print(data)
+#print(data)
 # Открываем CSV файл для записи
-#output_file_name = 'output.csv'
-#with open(output_file_name, 'w', newline='', encoding='utf-8') as output_file:
-#    csv_writer = csv.writer(output_file)
+output_file_name = 'output.csv'
+with open(output_file_name, 'w', newline='', encoding='utf-8') as output_file:
+    csv_writer = csv.writer(output_file)
 
     # Записываем заголовки столбцов
-#    csv_writer.writerow(['Дата', 'Имена'])
+    csv_writer.writerow(['РБУ', 'Нас.пункт', "Дата", "Имя", "Фамилия","Отец", "Мать", "Девичья фам. матери"])
 
     # Записываем данные
-#    csv_writer.writerows(data)
+    csv_writer.writerows(data)
 
 #print(f'Данные успешно записаны в {output_file_name}')
